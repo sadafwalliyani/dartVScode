8ae30fa6462e360d6733132b3cdb70052ab126d2
sadaf walliyani <sadafwalliyani@gmail.com>
2022-03-14 08:14:45 +0500

    task completed

---
+++ LogicalOperators.dart
@@ -0,0 +1,63 @@
+void main() {
+
+ var lst = new List(3); 
+   lst[0] = 12; 
+   lst[1] = 13; 
+   lst[2] = 11; 
+   print(lst);  
+   
+/*bool test; 
+   test = 12 > 5; 
+   print(test); 
+
+String str1="Assignment-3";
+String str2= "'is very interesting'";
+print("$str1 $str2");
+
+
+// Integer
+   int num1 = 2; 
+// double value
+   double num2 = 1.5; 
+// print the values
+   print(num1);
+   print(num2);
+   
+
+List<int> first = [1,2,3,4,5,6,7];
+List<int> second = [3,5,6,7,9,10];
+List<int> difference = first.toSet().difference(second.toSet()).toList();
+print(difference.toString());
+// prints [1, 2, 4]
+
+/ Conditional expressions:
+//  condition ? Exp1 : exp2
+int a=2;
+int b=3;
+
+if (a<b){
+  print("$a is smaller");
+}
+else {
+  print("$b is smaller");
+}
+a<b ? print ("$a is smaller"): print("$b is smaller");
+
+
+//Price of each ticket  
+var ticket=600;
+var res=ticket*5;
+
+//COst of 5 tickets 
+print("5 Ticket cost is= $res");
+var a=10;
+var b=20;
+//var res=(a<b)&&(b>a);
+//var res=(a<b)||(b>a);
+var res=(a<b)!=(b>a);
+print("Not equal Operator:: $res");
+duble  n = 2.20; 
+   var num = n is! int; 
+   print(num); */
+
+   }
