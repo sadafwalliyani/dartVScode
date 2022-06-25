f49e8eac822a820b7d5f1002abe51a6eb7efe74c
sadafwalliyani <sadafwalliyani@gmail.com>
2022-06-25 15:16:06 +0500

    Task Done

---
+++ PracticeDart.dart
@@ -0,0 +1,14 @@
+void main(){
+  var myList=[0,4,6,9,3,2,8,10];
+  myList.sort();
+  print(myList);
+  var myList2=['sadaf','rayyan','rustam','abrish','zayyan'];
+  myList2.sort();
+  print(myList2);
+
+  var vals=[];
+  if(vals.isEmpty){
+    print("my list is empty");
+    
+  }
+}
